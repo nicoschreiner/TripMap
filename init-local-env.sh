28c908308e54e0b3c8ed2b7f98ca192cee96425c
Nico Schreiner <nico@nicoschreiner.de>
2018-07-14 23:48:50 +0200

    Add files for local development.

---
+++ init-local-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$PROJECT_PATH="$(pwd)/src"
+
+echo "Install dependencies ..."
+sudo docker run --rm -v "${PROJECT_PATH}":/app composer/composer install
+sudo docker run --rm -v "${PROJECT_PATH}":/usr/src/app -w /usr/src/app node npm install
+
+echo "Set permissions ..."
+sudo chown -R $(whoami):$(whoami) "${PROJECT_PATH}"
+sudo chmod -R 777 "${PROJECT_PATH}/storage" "${PROJECT_PATH}/bootstrap/cache"
+
+exit 0
